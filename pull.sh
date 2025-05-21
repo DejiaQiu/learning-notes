2e3bdd98dbafd6e926185b6d5fefb60bf3734f0d
邱德佳 <798951257@qq.com>
2025-05-21 23:39:00 +0800

    新增时间戳

--- pull.sh
+++ pull.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+msg=$1
+if [ -z "$msg" ]; then
+    msg="更新于 $(date '+%Y-%m-%d %H:%M:%S')"
+fi
 git add .
-git commit -m "$1"
+git commit -m "$msg"
 git push origin-github main
 git push origin-gitee main
